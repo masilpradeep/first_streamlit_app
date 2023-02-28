855d71b3f4fd172c33e9f181d85ca31ac53e9679
masilpradeep <61133195+masilpradeep@users.noreply.github.com>
2023-02-27 23:16:54 -0500

    Added menu with pic

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,9 @@ import streamlit
 
 streamlit.title("my parents new healthy dinner")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Idily & Sambar")
-streamlit.text("Rice and Parupu")
-streamlit.text("Egg and Chicken")
-
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
